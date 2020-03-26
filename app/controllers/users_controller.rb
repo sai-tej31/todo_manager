5f73b7d50f3fbd360aca791654af3bd51cfca173
sai-tej31 <saitejareddy31@gmail.com>
2020-03-26 15:36:52 +0530

    new user created with post request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    render plain: User.order(:id).map { |user| user.to_pleasant_string }.join("\n")
+  end
+
+  def show
+    id = params[:id]
+    render plain: User.find(id).to_pleasant_string
+  end
+
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    newuser = User.create(
+      name: name,
+      email: email,
+      password: password,
+    )
+    render plain: "new user created with #{newuser.id}"
+  end
 end
